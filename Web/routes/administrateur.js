2189b9e8b153b8434aa0fd18f659b71cd8701f30
JulieMlr <julie.miler.jm@gmail.com>
2021-05-07 12:12:38 +0200

    mise en page + trie class inutile

--- Web/routes/administrateur.js
+++ Web/routes/administrateur.js
@@ -6,13 +6,13 @@ const bcrypt = require('bcrypt');
 const Administrateur = require('../models/Administrateurs');
 const Utilisateur = require('../models/Utilisateurs');
 
+
+/*Page Accueil */ 
 router.get('/', (req, res) => {
-  /*Administrateur.find()
-    .then((administrateurs) => res.send(administrateurs))
-    .catch((err) => console.log(err));*/
   res.sendFile(path.resolve('accueil.html'));
 });
 
+/* Pour supprimer un Utilisateur */
 router.get('/delete/:_idAdmin/:_id', (req, res) => {
   const { _id } = req.params;
   const { _idAdmin } = req.params;
@@ -21,6 +21,7 @@ router.get('/delete/:_idAdmin/:_id', (req, res) => {
     .catch((err) => console.log(err));
 });
 
+/* Pour modifier un Utilisateur */
 router.get('/modifier/:_idAdmin/:_id', (req, res) => {
   const { _id } = req.params;
   const { _idAdmin } = req.params;
@@ -38,14 +39,17 @@ router.get('/modifier/:_idAdmin/:_id', (req, res) => {
   });
 });
 
+/* Page inscription Admin*/
 router.get('/inscriptionAdmin', (req, res) => {
   res.sendFile(path.resolve('inscription.html'));
 });
 
+/* Page connexion Admin */
 router.get('/connexionAdmin', (req, res) => {
   res.sendFile(path.resolve('connexion.html'));
 });
 
+/* Page mon compte Admin */
 router.get('/modifierAdmin/:_id', (req, res) => {
   const { _id } = req.params;
   Administrateur.findOne({ _id: _id }).then((administrateur) => {
@@ -58,6 +62,8 @@ router.get('/modifierAdmin/:_id', (req, res) => {
     });
   });
 });
+
+/* Modifier Admin (nos informations) */
 router.post('/modifierAdmin', async (req, res) => {
   const idAdmin = req.body.admin_id;
   const nom = req.body.user_nom;
@@ -105,6 +111,7 @@ router.post('/modifierAdmin', async (req, res) => {
   }
 });
 
+/* Inscription Admin */
 router.post('/inscriptionAdmin', async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(req.body.user_password, salt);
@@ -135,21 +142,7 @@ router.post('/inscriptionAdmin', async (req, res) => {
     });
 });
 
-router.post('/put', (req, res) => {
-  const idAdmin = req.body.admin_id;
-  const id = req.body.user_id;
-  const nom = req.body.user_nom;
-  const prenom = req.body.user_prenom;
-  const email = req.body.user_email;
-  const date = req.body.user_date;
-  Utilisateur.findOneAndUpdate(
-    { _id: id },
-    { $set: { nom: nom, prenom: prenom, email: email, dateDeNaissance: date } }
-  )
-    .then((utilisateurs) => res.redirect('/administrateur/' + idAdmin))
-    .catch((err) => console.log(err));
-});
-
+/* Page Accueil connecté */
 router.get('/:_id', (req, res) => {
   const { _id } = req.params;
   Utilisateur.find()
@@ -168,6 +161,7 @@ router.get('/:_id', (req, res) => {
     .catch((err) => console.log(err));
 });
 
+/* Connexion Admin */
 router.post('/connexion', (req, res) => {
   const email = req.body.user_mail;
   const motDePasse = req.body.user_password;
@@ -188,44 +182,6 @@ router.post('/connexion', (req, res) => {
               res.render('connexion.html', {
                 erreur: 'Mot de passe incorrect',
               });
-            }
-          }
-        );
-      }
-    })
-    .catch((err) => res.send(err));
-});
-router.put('/:_id', (req, res) => {
-  const { _id } = req.params;
-  const modifyUser = req.body;
-  Administrateur.findOneAndUpdate({ _id }, { $set: modifyUser })
-    .then((administrateurs) => res.send('Admin Updated'))
-    .catch((err) => console.log(err));
-});
-
-router.delete('/:_id', (req, res) => {
-  const { _id } = req.params;
-  Administrateur.findOneAndDelete({ _id: _id })
-    .then((administrateurs) => res.send('success'))
-    .catch((err) => console.log(err));
-});
-
-router.post('/detail', async (req, res) => {
-  const email = req.body.user_mail;
-  const motDePasse = req.body.user_password;
-  Administrateur.findOne({ email: email })
-    .then((administrateurs) => {
-      if (administrateurs == null) {
-        res.sendFile(path.resolve('connexion.html'));
-      } else {
-        bcrypt.compare(
-          motDePasse,
-          administrateurs.motDePasse,
-          function (err, response) {
-            if (response == true) {
-              res.send('Hello');
-            } else {
-              res.sendFile(path.resolve('connexion.html'));
             }
           }
         );
